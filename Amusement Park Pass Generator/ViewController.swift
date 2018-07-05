b5fe5e8e6324d8fced7e090700bc8b01341360e6
tdimnet <thomas.dimnet@gmail.com>
2018-07-05 19:50:28 +0200

    Style the bottom of the form:
    - Add and style the two buttons.
    - Update the view controller by extending the type of the IB.

--- Amusement Park Pass Generator/ViewController.swift
+++ Amusement Park Pass Generator/ViewController.swift
@@ -8,6 +8,44 @@
 
 import UIKit
 
+
+/*
+ * Code snippet taken from stackoverflow.
+ * Add border-width, border-radius and border-color for the IB.
+ * link of the code snippet: https://stackoverflow.com/questions/28854469/change-uibutton-bordercolor-in-storyboard
+ */
+@IBDesignable extension UIButton {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
