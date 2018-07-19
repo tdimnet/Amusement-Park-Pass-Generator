44dac2b7de993eb8f2c279b3e5a03f414ecf2c17
tdimnet <thomas.dimnet@gmail.com>
2018-07-19 18:17:35 +0200

    Code refactoring.

---
+++ Amusement Park Pass Generator/Entrants/Vendor.swift
@@ -0,0 +1,9 @@
+//
+//  Vendor.swift
+//  Amusement Park Pass Generator
+//
+//  Created by Thomas Dimnet on 19/07/2018.
+//  Copyright © 2018 Thomas Dimnet. All rights reserved.
+//
+
+import Foundation
